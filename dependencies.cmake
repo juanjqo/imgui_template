d0ea6c0683e247b4ccb19c9e0a0562b65124dcb2
Juancho <juanjqogm@gmail.com>
2024-03-12 13:26:00 +0900

    Fixed bugs in Ubuntu

--- dependencies.cmake
+++ dependencies.cmake
@@ -139,9 +139,14 @@ set(IMPLOT_SRC
     ${IMPLOT_DIR}/implot_demo.cpp
 )
 
-add_library(juangui_wrapper ${COMMONS_DIR}/juangui_wrapper.cpp)
+
 add_library(imgui ${IMGUI_HEADERS} ${IMGUI_SRC})
 add_library(implot ${IMPLOT_HEADERS} ${IMPLOT_SRC})
-
+add_library(juangui_wrapper ${COMMONS_DIR}/juangui_wrapper.cpp)
+target_link_libraries(juangui_wrapper
+    imgui
+    implot
+    glfw
+    OpenGL::GL)
 
 
